9e5a61d4dee868356dc9720d5522a48c10efad62
varuka999 <vjkpokemon@yahoo.com>
2026-06-10 18:18:29 -0700

    Pre W10 2

--- VGP242/06_HelloDebug/GameState.cpp
+++ VGP242/06_HelloDebug/GameState.cpp
@@ -17,25 +17,73 @@ void GameState::Update(float deltaTime)
 {
     UpdateCamera(deltaTime);
 }
+
+enum class Shape
+{
+    None,
+    AABB,
+    AABBFilled,
+    Sphere,
+    GroundPlane,
+    GroundCircle,
+    Transform,
+};
+const char* gShapeNames[] =
+{
+    "None",
+    "AABB",
+    "AABBFilled",
+    "Sphere",
+    "GroundPlane",
+    "GroundCircle",
+    "Transform"
+};
+Shape gCurrentShape = Shape::None;
+Color gShapeColor = Colors::White;
+float gPlaneSize = 1.0f;
+float gRingSize = 16.0f;
 void GameState::Render()
 {
-    SimpleDraw::AddGroundPlane(10.0f, Colors::White);
+    switch (gCurrentShape)
+    {
+    case Shape::None:
+        break;
+    case Shape::AABB:
+        SimpleDraw::AddAABB(1.0f, 1.0f, 1.0f, gPlaneSize, gPlaneSize, gPlaneSize, gShapeColor);
+        break;
+    case Shape::AABBFilled:
+        SimpleDraw::AddFilledAABB(1.0f, 1.0f, 1.0f, gPlaneSize, gPlaneSize, gPlaneSize, gShapeColor);
+        break;
+    case Shape::Sphere:
+        SimpleDraw::AddSphere(gRingSize, gRingSize, gPlaneSize, {0.0f, 0.0f, 0.0f}, gShapeColor);
+        break;
+    case Shape::GroundPlane:
+        SimpleDraw::AddGroundPlane(gPlaneSize, gShapeColor);
+        break;
+    case Shape::GroundCircle:
+        SimpleDraw::AddGroundCircle(gRingSize, gPlaneSize, { 0.0f, 0.0f, 0.0f }, gShapeColor);
+        break;
+    case Shape::Transform:
+        //SimpleDraw::AddTransform(mMatrix)
+        break;
+    default:
+        break;
+    }
+
     SimpleDraw::Render(mCamera);
 }
-float myVariable = 0.0f;
 void GameState::DebugUI()
 {
     ImGui::Begin("Debug", nullptr, ImGuiWindowFlags_AlwaysAutoResize);
-    // content
-    ImGui::Text("Hello Debug Window");
-    if (ImGui::DragFloat("MyVariable", &myVariable))
+    // CONTENT GOES BETWEEN BEGIN AND END
+    ImGui::ColorEdit4("ShapeColor", &gShapeColor.r);
+    int currentShape = (int)gCurrentShape;
+    if (ImGui::Combo("Shape", &currentShape, gShapeNames, std::size(gShapeNames)))
     {
-        LOG("MY VARIABLE UPDATE %f", myVariable);
+        gCurrentShape = (Shape)currentShape;
     }
-    if (ImGui::Button("MyButton"))
-    {
-        LOG("BUTTON PRESSED");
-    }
+    ImGui::DragFloat("PlaneSize", &gPlaneSize, 1.0f, 1.0f, 10000.0f);
+    ImGui::DragFloat("RingSize", &gRingSize, 1.0f, 1.0f, 10000.0f);
     ImGui::End();
 }
 void GameState::UpdateCamera(float deltaTime)
